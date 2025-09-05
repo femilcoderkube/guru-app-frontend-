728215797763492d068f7b8cd0061938ec365e84
moinck <rajrana.coderkube@gmail.com>
2025-09-05 17:49:47 +0530

    bug fixed

---
+++ src/Componets/animation.jsx
@@ -0,0 +1,44 @@
+// LTR And RTL Start
+export const leftToRight = {
+    hidden: { opacity: 0, x: -100 },
+    visible: (i = 1) => ({
+      opacity: 1,
+      x: 0,
+      transition: {
+        delay: i * 0.3,
+        type: "spring",
+        stiffness: 60,
+      },
+    }),
+  };
+  
+  export const rightToLeft = {
+    hidden: { opacity: 0, x: 100 },
+    visible: (i = 1) => ({
+      opacity: 1,
+      x: 0,
+      transition: {
+        delay: i * 0.3,
+        type: "spring",
+        stiffness: 60,
+      },
+    }),
+  };
+  // LTR And RTL end
+
+  // card animation start
+  export const cardVariantsAni = {
+  hidden: { opacity: 0, y: 30 },
+  visible: (index) => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: index * 0.1,
+      duration: 0.3,
+      ease: "easeOut",
+    },
+  }),
+};
+// card animation End
+
+  
